0ded2697291886085c0e59fca5bbeb1527366067
kawamotonokisaki <kawamoto@nokisaki.com>
2024-08-28 19:01:20 +0900

    2

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -85,7 +85,7 @@ const HomeComponent = () => {
               type="submit"
               className="mt-4 w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Fetch Todo
+              Fetch Todo!
             </button>
           </form>
 
